f917441f68a9b588ac6cca59d715e90baebc37a4
Michael Boyd <michael_boyd@live.co.uk>
2025-06-26 21:15:31 +0100

    client initial commit

---
+++ client/tailwind.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      keyframes: {
+        loader: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+      animation: {
+        loader: 'loader 2s linear infinite',
+      },
+    },
+  },
+};
