1d44b3a9ede8595b2e65354cbe3b7b812b02baa3
Michael Boyd <michaelb@forrit.com>
2025-07-01 20:03:40 +0100

    layout changes

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
-	declarations: [ContainerComponent],
+	declarations: [],
 	imports: [CommonModule],
-	exports: [ContainerComponent],
+	exports: [],
 })
 export class SharedModule {}
 
