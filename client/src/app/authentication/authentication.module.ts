2d13816888ad732f0159cd1518d489e00e47c71e
Michael Boyd <michaelb@forrit.com>
2025-08-01 10:45:46 +0100

    authentication loading on button

--- client/src/app/authentication/authentication.module.ts
+++ client/src/app/authentication/authentication.module.ts
@@ -3,10 +3,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthenticationComponent } from './authentication.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
 	declarations: [AuthenticationComponent],
-	imports: [FormsModule, ReactiveFormsModule, CommonModule],
+	imports: [FormsModule, ReactiveFormsModule, CommonModule, SharedModule],
 	exports: [AuthenticationComponent],
 })
 export class AuthenticationModule {
