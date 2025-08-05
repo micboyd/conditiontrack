dd6b09f8e5850f1f2620ca45c8a888f463a91830
Michael Boyd <michaelb@forrit.com>
2025-08-05 17:00:25 +0100

    new conditioning models

--- client/src/app/conditioning/conditioning.module.ts
+++ client/src/app/conditioning/conditioning.module.ts
@@ -1,14 +1,15 @@
+import { AppRoutingModule } from '../app-routing.module';
 import { ConditioningComponent } from './conditioning.component';
+import { ConditioningLibraryComponent } from './conditioning-library/conditioning-library.component';
+import { ConditioningRecordsComponent } from './conditioning-records/conditioning-records.component';
+import { EditRecordComponent } from './conditioning-records/edit-record/edit-record.component';
+import { EditSessionComponent } from './conditioning-library/edit-session/edit-session.component';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { ConditioningRecordsComponent } from './conditioning-records/conditioning-records.component';
-import { ConditioningLibraryComponent } from './conditioning-library/conditioning-library.component';
-import { EditSessionComponent } from './conditioning-library/edit-session/edit-session.component';
-import { EditRecordComponent } from './conditioning-records/edit-record/edit-record.component';
 
 @NgModule({
 	declarations: [ConditioningComponent, ConditioningRecordsComponent, ConditioningLibraryComponent, EditSessionComponent, EditRecordComponent],
-	imports: [SharedModule],
+	imports: [SharedModule, AppRoutingModule],
 	exports: [ConditioningComponent],
 })
 export class ConditioningModule {
