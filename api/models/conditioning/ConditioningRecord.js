fc809687f2d0d22155ce2fb525016e8c4436553f
Michael Boyd <michaelb@forrit.com>
2026-04-14 17:11:06 +0100

    allow 0 for duration

--- api/models/conditioning/ConditioningRecord.js
+++ api/models/conditioning/ConditioningRecord.js
@@ -6,7 +6,7 @@ const conditioningRecordSchema = new mongoose.Schema(
 		userId: { type: String, required: true }, // reference to user
 		sessionId: { type: String, required: true },
 		date: { type: Date, required: true, default: Date.now }, // when the session took place
-		duration: { type: Number, required: true, min: 1 }, // actual time spent (minutes)
+		duration: { type: Number, required: true, min: 0 }, // actual time spent (minutes)
 		notes: { type: String, default: '' }, // user’s personal notes or feedback
 		completed: { type: Boolean, default: false }, // whether the session was finished
 		caloriesBurned: { type: Number, default: 0, min: 0 },
