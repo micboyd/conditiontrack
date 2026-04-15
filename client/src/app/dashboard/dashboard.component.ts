fb6944c763a7c1bd4afb3e8a5660d91972ac4680
Michael Boyd <michaelb@forrit.com>
2026-04-15 15:12:18 +0100

    close on save

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -64,6 +64,7 @@ export class DashboardComponent implements OnInit {
 	// Workout logging state
 	logWorkoutStep: 1 | 2 = 1;
 	logWorkoutSelectedWorkout: Workout | null = null;
+	workoutCloseOnSave = true;
 	workoutLogForm!: FormGroup;
 	workoutLogErrors: string[] = [];
 	workoutLogLoading = false;
@@ -652,7 +653,9 @@ export class DashboardComponent implements OnInit {
 		this.workoutRecordService.createWorkoutRecord(payload).subscribe((record) => {
 			this.workoutRecords = [...this.workoutRecords, record];
 			this.workoutLogLoading = false;
-			this.logWorkoutDrawer.close();
+			if (this.workoutCloseOnSave) {
+				this.logWorkoutDrawer.close();
+			}
 		});
 	}
 
