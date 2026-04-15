a2307c08d931135416d03d821f60eb5addfa83ac
Michael Boyd <michaelb@forrit.com>
2026-04-15 16:16:13 +0100

    scrolls to the top when router changes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -170,7 +170,7 @@ const routes: Routes = [
 	{ path: '**', redirectTo: 'dashboard' },
 ];
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
 	exports: [RouterModule],
 })
 export class AppRoutingModule {}
